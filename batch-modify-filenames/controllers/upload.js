bc359635017358d791ff26c3e621857cd8e3de8b
gating <674964222@qq.com>
2020-07-03 14:58:45 +0800

    去除get的测试接口

--- batch-modify-filenames/controllers/upload.js
+++ batch-modify-filenames/controllers/upload.js
@@ -1,52 +1,38 @@
 // 压缩文件
 const archiver = require("archiver");
 const Stream = require("stream");
+// 判断是否为数组，如果不是，则转为数组
 const isArray = (arr) => {
   if (!Array.isArray(arr)) {
     arr = [arr];
   }
-
   return arr;
 };
+// 上传接口
 exports.upload = async (ctx) => {
+  // 获取上传的文件
   let { files } = ctx.request.files;
+  // 获取上传的文件名
   let filenames = isArray(ctx.request.body.name);
+  // 将文件转为数组
   files = isArray(files);
+  // 设置响应头，告诉浏览器我要下载的文件叫做files.zip
+  // attachment用于浏览器文件下载
   ctx.attachment("files.zip");
+  // 设置响应头的类型
   ctx.set({ "Content-Type": "application/zip" });
+  // 定义一个双向流
   const stream = new Stream.PassThrough();
+  // 把流返回给前端
   ctx.body = stream;
-
   // 压缩成zip
   const archive = archiver("zip", {
     zlib: { level: 9 }, // Sets the compression level.
   });
   archive.pipe(stream);
-
   for (let i = 0; i < files.length; i++) {
     const path = files[i].path;
     archive.file(path, { name: filenames[i] });
-  }
-
-  archive.finalize();
-};
-
-exports.test = async (ctx) => {
-  ctx.set("Content-Type", "application/zip");
-  const archive = archiver("zip", {
-    zlib: { level: 9 },
-  });
-
-  const stream = new Stream.PassThrough();
-  ctx.body = stream;
-
-  archive.pipe(stream);
-
-  for (let i = 0; i < 2; i++) {
-    archive.file(
-      "C:\\Users\\gating\\Desktop\\demo\\batch-modify-filenames\\uploads\\2020073\\e8afd953-2a05-45d9-a9a7-2189678d08da",
-      { name: `hello${i}.txt` }
-    );
   }
   archive.finalize();
 };
--- batch-modify-filenames/routes/upload.js
+++ batch-modify-filenames/routes/upload.js
@@ -1,10 +1,8 @@
-const { upload, test } = require("../controllers/upload");
+const { upload } = require("../controllers/upload");
 const Router = require("koa-router");
 const router = new Router({
   prefix: "/upload",
 });
 
 router.post("/", upload);
-
-router.get("/", test);
 module.exports = router;
