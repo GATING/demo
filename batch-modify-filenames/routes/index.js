a6b46aaef34e2a666a40c8886da1b52ef03da140
gating <674964222@qq.com>
2020-07-03 15:05:16 +0800

    添加代码注释

--- batch-modify-filenames/routes/index.js
+++ batch-modify-filenames/routes/index.js
@@ -1,7 +1,10 @@
 const { resolve } = require("path");
+// 用于获取文件
 const glob = require("glob");
 module.exports = (app) => {
+  // 获取当前文件夹下的所有文件，除了自己
   glob.sync(resolve(__dirname, "!(index).js")).forEach((item) => {
+    // 添加路由
     const route = require(item);
     app.use(route.routes()).use(route.allowedMethods());
   });
--- batch-modify-filenames/routes/upload.js
+++ batch-modify-filenames/routes/upload.js
@@ -1,8 +1,12 @@
+// 导入控制器
 const { upload } = require("../controllers/upload");
+// 导入路由
 const Router = require("koa-router");
+// 设置路由前缀为 upload
 const router = new Router({
   prefix: "/upload",
 });
-
+// post请求，请求地址为 ip + 前缀 + '/'
 router.post("/", upload);
+// 导出路由
 module.exports = router;
