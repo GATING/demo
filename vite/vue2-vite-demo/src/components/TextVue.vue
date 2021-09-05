d64fd0d3d6606e9e521dc55d178bbea4dea91d26
unknown <674964222@qq.com>
2021-09-05 16:35:43 +0800

    feat:add vue2-vite-demo

---
+++ vite/vue2-vite-demo/src/components/TextVue.vue
@@ -0,0 +1,7 @@
+<script lang="jsx">
+export default {
+  render() {
+    return <div>我也是vue文件的jsx，且没有template</div>
+  }
+}
+</script>
---
+++ vite/vue2-vite-demo/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1 class="text-primary">123</h1>
+    <button @click="handleClick">点击我</button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@comp/HelloWorld'
+const { sum } = require('../utils/index')
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    handleClick() {
+      console.log(sum(1, 32))
+    }
+  }
+}
+</script>
